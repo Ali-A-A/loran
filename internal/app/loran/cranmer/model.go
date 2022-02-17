e002be71e813ad878f273a860771a9ca21633350
Ali-A-A <aliabbasi806@gmail.com>
2022-02-17 12:45:57 -0800

    Add request tags

--- internal/app/loran/cranmer/model.go
+++ internal/app/loran/cranmer/model.go
@@ -4,6 +4,6 @@ package cranmer
 // UserID represents unique user that
 // we want to increment count based on EntityID.
 type AddRequest struct {
-	UserID   int32
-	EntityID int64
+	UserID   int32 `json:"user_id"`
+	EntityID int64 `json:"entity_id"`
 }
